bb51f8d6edb34c510a2bbaa6cf802bed5dd319d7
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-01-10 21:30:55 +0400

    add form

--- src/js/components/swiper/stagesSwiper.js
+++ src/js/components/swiper/stagesSwiper.js
@@ -3,11 +3,19 @@ import "swiper/css/bundle";
 import { rem } from "../../utils/constants";
 
 const stagesSwiper = () => {
+const sliders = document.querySelectorAll('.stages__swiper-slide')
+let rows = Math.ceil(sliders.length/3)
+const nums = document.querySelectorAll('.stages__swiper-slide-num')
+nums.forEach((item, i) => {
+  item.textContent = (i + 1).toString().padStart(2, '0');
+})
+
+
     const swiper = new Swiper(".stages__swiper", {
         slidesPerView: 3,
         // spaceBetween: 30,
         grid: {
-          rows: 2,
+          rows: rows,
         },
         scrollbar: {
             el: ".stages__swiper-scrollbar",
