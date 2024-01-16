002593cfe5b13970a09a5a75a0ad01caf3294c89
NastiaCandor <andubova98@gmail.com>
2024-01-16 17:50:38 +0300

    feat: add popup range

--- src/js/components/swiper/swipers.js
+++ src/js/components/swiper/swipers.js
@@ -190,4 +190,122 @@ const popupCertSwiper = () => {
   });
 }
 
-export {catalogSwiper, productBannerSwiper, benefitsSwiper, ourWorksTabSwiper, specialistsSwiper, weDoingSwiper, answersSwiper, popupCertSwiper};
+// RANGE POPUP
+const rangePopupSwiper = () => {
+  const thumbSwiperOpt = {
+    slidesPerView: 5,
+    spaceBetween: rem(2),
+    speed: 500,
+    loop: true,
+    freeMode: true,
+    watchSlidesProgress: true,
+  }
+  const thumbSwiper1 = new Swiper('.range-popup__thumb-swiper--1', thumbSwiperOpt);
+  const swiperInner1Pop = new Swiper('.range-popup__swiper-slide-swiper--1', {
+    spaceBetween: rem(2),
+    speed: 300,
+    loop: true,
+    navigation: {
+        nextEl: '.popup-range__swiper-button-next',
+        prevEl: '.popup-range__swiper-button-prev',
+    },
+    thumbs: {
+      swiper: thumbSwiper1,
+    },
+  });
+  $('.range__swiper-slide-swiper--1 .range__swiper-slide-swiper-slide-img-box').click(function(event) {
+    swiperInner1Pop.slideTo($(this).attr('data-index'))
+  });
+
+  const thumbSwiper2 = new Swiper('.range-popup__thumb-swiper--2', thumbSwiperOpt);
+  const swiperInner2 = new Swiper('.range-popup__swiper-slide-swiper--2', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    navigation: {
+        nextEl: '.popup-range__swiper-button-next',
+        prevEl: '.popup-range__swiper-button-prev',
+    },
+    thumbs: {
+      swiper: thumbSwiper2,
+    },
+  })
+  $('.range__swiper-slide-swiper--2 .range__swiper-slide-swiper-slide-img-box').click(function(event) {
+    swiperInner2.slideTo($(this).attr('data-index'))
+  });
+
+  const thumbSwiper3 = new Swiper('.range-popup__thumb-swiper--3', thumbSwiperOpt);
+  const swiperInner3 = new Swiper('.range-popup__swiper-slide-swiper--3', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    navigation: {
+        nextEl: '.popup-range__swiper-button-next',
+        prevEl: '.popup-range__swiper-button-prev',
+    },
+    thumbs: {
+      swiper: thumbSwiper3,
+    },
+  })
+  $('.range__swiper-slide-swiper--3 .range__swiper-slide-swiper-slide-img-box').click(function(event) {
+    swiperInner3.slideTo($(this).attr('data-index'))
+  });
+
+  const thumbSwiper4 = new Swiper('.range-popup__thumb-swiper--4', thumbSwiperOpt);
+  const swiperInner4 = new Swiper('.range-popup__swiper-slide-swiper--4', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    navigation: {
+        nextEl: '.popup-range__swiper-button-next',
+        prevEl: '.popup-range__swiper-button-prev',
+    },
+    thumbs: {
+      swiper: thumbSwiper4,
+    },
+  })
+  $('.range__swiper-slide-swiper--4 .range__swiper-slide-swiper-slide-img-box').click(function(event) {
+    swiperInner4.slideTo($(this).attr('data-index'))
+  });
+
+  const thumbSwiper5 = new Swiper('.range-popup__thumb-swiper--5', thumbSwiperOpt);
+  const swiperInner5 = new Swiper('.range-popup__swiper-slide-swiper--5', {
+    slidesPerView: 1,
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    speed: 300,
+    navigation: {
+        nextEl: '.popup-range__swiper-button-next',
+        prevEl: '.popup-range__swiper-button-prev',
+    },
+    thumbs: {
+      swiper: thumbSwiper5,
+    },
+  })
+  $('.range__swiper-slide-swiper--5 .range__swiper-slide-swiper-slide-img-box').click(function(event) {
+    swiperInner5.slideTo($(this).attr('data-index'))
+  });
+
+  const navItems = document.querySelectorAll('.range__swiper-nav-item');
+  const popupSlides = document.querySelectorAll('.range-popup__swiper-slide');
+  let currInd = 0;
+
+  // Обработчик клика на элемент навигации
+  navItems.forEach((item, index) => {
+      item.addEventListener('click', function () {
+          popupSlides[currInd].classList.remove('active');
+          popupSlides[index].classList.add('active');
+          currInd = index;
+      });
+  });
+
+};
+
+export {catalogSwiper, productBannerSwiper, benefitsSwiper, ourWorksTabSwiper, specialistsSwiper, weDoingSwiper, answersSwiper, popupCertSwiper, rangePopupSwiper};
